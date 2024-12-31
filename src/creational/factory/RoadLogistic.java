1cb9c96ac736cbcc569508572bf821489025b76e
Nhan Van Truong <truongv.nhan2812@gmail.com>
2024-12-31 09:37:40 +0700

    refactor

--- src/creational/factory/RoadLogistic.java
+++ src/creational/factory/RoadLogistic.java
@@ -2,11 +2,8 @@ package creational.factory;
 
 public class RoadLogistic extends Logistics {
 
-  private Transport transport;
-
   @Override
   public Transport createTransport() {
-    transport = new TruckDelivery();
-    return transport;
+    return new TruckDelivery();
   }
 }
--- src/creational/factory/SeaLogistic.java
+++ src/creational/factory/SeaLogistic.java
@@ -2,11 +2,8 @@ package creational.factory;
 
 public class SeaLogistic extends Logistics {
 
-  private Transport transport;
-
   @Override
   public Transport createTransport() {
-    transport = new ShipDelivery();
-    return transport;
+    return new ShipDelivery();
   }
 }
